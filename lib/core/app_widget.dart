e428df9f1a883e05562f2f95a46af00b55914fa7
Daniel Schmitz <danieljfa@gmail.com>
2021-04-24 11:52:09 -0300

    Navegação da splash pra home

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,9 +1,10 @@
 import 'package:devquiz/home/home_page.dart';
+import 'package:devquiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "DevQuiz", home: HomePage());
+    return MaterialApp(title: "DevQuiz", home: SplashPage());
   }
 }
--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,14 +1,21 @@
 import 'package:devquiz/core/app_gradients.dart';
 import 'package:devquiz/core/app_images.dart';
+import 'package:devquiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 1)).then((value) =>
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage())));
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(gradient: AppGradients.linear),
-        child: Center(child: Image.asset(AppImages.logo),),
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
       ),
     );
   }
